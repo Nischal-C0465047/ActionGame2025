782f6c18e012c62a0584ffdfa089c7b2824d9457
Nischal <C0465047@online.camosun.ca>
2025-03-11 17:04:07 -0700

    Initial Commit

---
+++ ActionGame2025Tests/ActionGame2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ActionGame2025Tests.swift
+//  ActionGame2025Tests
+//
+//  Created by Nischal Karki on 2025-03-11.
+//
+
+import Testing
+@testable import ActionGame2025
+
+struct ActionGame2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ActionGame2025UITests/ActionGame2025UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ActionGame2025UITestsLaunchTests.swift
+//  ActionGame2025UITests
+//
+//  Created by Nischal Karki on 2025-03-11.
+//
+
+import XCTest
+
+final class ActionGame2025UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
